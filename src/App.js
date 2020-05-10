aae7e853e2958ba16e986050b06d540834d7e569
lateralcreativity <lateralcreativity@outlook.com>
2020-05-10 13:18:20 -0500

    Removed timer hook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(32);
   const [awayScore, setAwayScore] = useState(32);
-  const [timer, setTimer] = useState(0);
 
   const touchdownHandler = (event) => {
     if(event.target.className === "homeButtons__touchdown"){
